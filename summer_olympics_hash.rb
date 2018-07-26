c620612ac1b185426019f54c1f8dd0ba8ba7ac74
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-26 14:18:39 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -5,12 +5,12 @@ end
 
 def add_a_key_value_pair
    summer_olympics = {Sydney => 2000, Athens => 2004, Benijing => 2008 London => 2012}
-   summer_olympics {1996}
+   summer_olympics { Atlanta => 1996}
 end
 
 def iterate_through_hash
    summer_olympics = {Sydney => 2000, Athens => 2004, Benijing => 2008 London => 2012}.each do
-   summer_olympics {1996}.each do  
+   summer_olympics {Atlanta => 1996}.each do  
    puts "The 1996 summer olympics took place in Atlanta"
 end
 
